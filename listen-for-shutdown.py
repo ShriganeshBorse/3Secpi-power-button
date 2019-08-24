4194ce8f2e9e04325ae449d44f488dbf05b906d5
Shriganesh Borse <shriganesh.borse@gmail.com>
2019-08-24 17:16:13 +0530

    Change to 3sec press time before shutdown
    Change to 3sec press time before shutdown

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 
-
 import RPi.GPIO as GPIO
 import subprocess
+import time
 
+def Shutdown(channel):
+    start_time = time.time()
+    while GPIO.input(channel) == 0:
+        pass
+    press_time = time.time()-start_time
+
+    if press_time > 3:
+        print("Shutting Down")
+        subprocess.call(['shutdown', '-h', 'now'],shell=False)
+    else:
+        print("Ignoring System shutdown button, not long enough signal")
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.wait_for_edge(3, GPIO.FALLING)
+GPIO.add_event_detect(3, GPIO.FALLING, callback=Shutdown, bouncetime=500)
 
-subprocess.call(['shutdown', '-h', 'now'], shell=False)
+while 1:
+    time.sleep(1)
